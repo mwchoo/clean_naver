3723392ab6a108b9885024f6e7b355f32e589198
Minwoo Choo/추민우 <38900695+mwchoo@users.noreply.github.com>
2020-04-17 03:57:35 +0900

    [Complete] User Script

---
+++ CleanNaver.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         CleanNaver
+// @namespace    https://github.com/mwchoo/clean_naver
+// @version      0.1
+// @description  user script for clean naver (prevent ad, fix css)
+// @author       Minwoo Choo
+// @match        https://www.naver.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    // for naver new ui (2020-04-17)
+    const styles = `
+    .column_right.is_bfixed .column_fix_wrap {
+				position: fixed !important;
+        top: 70px !important;
+				bottom: auto !important;
+        margin-top: 20px !important;
+    }
+`;
+
+    const styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+    document.getElementsByClassName('sc_shopcast')[0].remove();
+})();
